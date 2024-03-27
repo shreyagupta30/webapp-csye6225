172fe27ccce5d8710f0293dd8eb71bb2fe32f054
shreyagupta30 <shreyagupta3011@gmail.com>
2024-03-27 17:00:59 -0400

    Fix correct spelling

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-from .views import UserAuthViewSet, GetUserAuthViewSet, UserEmailVerificationViewSet
+from .views import UserAuthViewSet, GetUserAuthViewSet, UserAuthVerificationViewSet
 
 urlpatterns = [
     path('v1/user', UserAuthViewSet.as_view(), name='user_auth'),
     path('v1/user/self', GetUserAuthViewSet.as_view(), name='user_self'),
-    path('v1/user/verify', UserEmailVerificationViewSet.as_view(), name='user_verify'),
+    path('v1/user/verify', UserAuthVerificationViewSet.as_view(), name='user_verify'),
 ]
