af104b8db65956efb11d149fe82269dca453fb8a
shreyagupta30 <shreyagupta3011@gmail.com>
2024-04-16 15:48:36 -0400

    demo

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from .views import UserAuthViewSet, GetUserAuthViewSet, UserAuthVerificationViewSet
 
 urlpatterns = [
-    path('v1/user', UserAuthViewSet.as_view(), name='user_auth'),
-    path('v1/user/self', GetUserAuthViewSet.as_view(), name='user_self'),
-    path('v1/user/verify', UserAuthVerificationViewSet.as_view(), name='user_verify'),
+    path('v2/user', UserAuthViewSet.as_view(), name='user_auth'),
+    path('v2/user/self', GetUserAuthViewSet.as_view(), name='user_self'),
+    path('v2/user/verify', UserAuthVerificationViewSet.as_view(), name='user_verify'),
 ]
